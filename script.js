a006d9515da9eacbf3d9deea9c77ddf00be1f31d
USER-1145-desgin <alexander.goldenleader.nope.not.me@gmail.com>
2025-10-20 17:17:37 -0400

    Fix weight calculation and update totalPercent divisor

--- script.js
+++ script.js
@@ -11,13 +11,12 @@ function calculate() {
   data.forEach(item => {
     const score = parseFloat(document.getElementById(item.s).value) || 0;
     const total = parseFloat(document.getElementById(item.t).value) || 1;
-    const weight = parseFloat(document.getElementById(item.w).value) || 0;\
+    const weight = parseFloat(document.getElementById(item.w).value) || 0;
 
     const percent = (score / total) * weight;
     document.getElementById(item.r).innerText = percent.toFixed(2) + "%";
     totalPercent += percent;
   });
-  const term = parseFloat(document.getElementById(tm).value) || 0;
-  totalPercent = totalPercent/term
+  totalPercent = totalPercent/70
   document.getElementById("result-box").innerText = "Your Result: " + totalPercent.toFixed(1) + "%";
 }
