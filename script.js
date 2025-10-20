e7feedfd867555aa9a21d18f51a2459ab8c1a480
USER-1145-desgin <alexander.goldenleader.nope.not.me@gmail.com>
2025-10-20 17:07:34 -0400

    Update script.js

--- script.js
+++ script.js
@@ -12,11 +12,12 @@ function calculate() {
     const score = parseFloat(document.getElementById(item.s).value) || 0;
     const total = parseFloat(document.getElementById(item.t).value) || 1;
     const weight = parseFloat(document.getElementById(item.w).value) || 0;
+    const Term Mark = parseFloat(document.getElementById(item.T).value) || 1;
 
     const percent = (score / total) * weight;
     document.getElementById(item.r).innerText = percent.toFixed(2) + "%";
     totalPercent += percent;
   });
-  totalPercent = totalPercent/70
+  totalPercent = totalPercent/Term Mark
   document.getElementById("result-box").innerText = "Your Result: " + totalPercent.toFixed(1) + "%";
 }
