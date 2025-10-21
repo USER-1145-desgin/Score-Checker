a73960de3582d95173082deff7d9c242c79e07e9
USER-1145-desgin <alexander.goldenleader.nope.not.me@gmail.com>
2025-10-20 22:06:41 -0400

    Calculate percent as a percentage of total weight

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ function calculate() {
     const total = parseFloat(document.getElementById(item.t).value) || 1;
     const weight = parseFloat(document.getElementById(item.w).value) || 0;
 
-    const percent = (score / total) * weight;
+    const percent = ((score / total) * weight)*100;
     document.getElementById(item.r).innerText = percent.toFixed(2) + "%";
     totalPercent += percent;
   });
